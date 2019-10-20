dc45f678299c044609cce13f77375d24d0acf117
Marcos H Polverini <marcospolverini@gmail.com>
2019-10-20 14:32:52 -0300

    Add JPA Annotations and change lombok to work with JPA

--- src/main/java/com/waes/diff/model/DiffBucket.java
+++ src/main/java/com/waes/diff/model/DiffBucket.java
@@ -1,21 +1,31 @@
 package com.waes.diff.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.util.StringUtils;
 
-import lombok.Builder;
-import lombok.Value;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Value
-@Builder(toBuilder = true)
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "diff_bucket")
 public final class DiffBucket {
-    
-    private final Integer id;
 
-    private final String right;
+    @Id
+    @Column(name = "diff_id", nullable = false)
+    private Integer id;
 
-    private final String left;
+    @Column(name = "diff_right", columnDefinition = "TEXT")
+    private String right;
 
-    public boolean isValid(){
+    @Column(name = "diff_left", columnDefinition = "TEXT")
+    private String left;
+
+    public boolean isValid() {
         return !StringUtils.isEmpty(right) && !StringUtils.isEmpty(left);
     }
 
